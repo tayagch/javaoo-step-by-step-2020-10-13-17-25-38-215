3968c17ab1fa31d9a09e872c65abf9906feb7899
TAYAGCH <tayagch@oocl.com>
2020-10-14 22:18:39 +0800

    [Practice06][Christian]update: update answer

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,30 @@
 package practice06;
 
-public class Teacher {
+import java.util.Optional;
+
+public class Teacher extends Person{
+
+    private int klass;
+
+    public Teacher(String name, int age,int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return klass != 0 ? super.introduce()+ " " + "I am a Teacher. I teach Class 2." : super.introduce()+ " " + "I am a Teacher. I teach No Class.";
+    }
 }
