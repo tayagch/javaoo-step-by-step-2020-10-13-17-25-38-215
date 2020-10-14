739b218caee265d9a4cd1e66b2c99cc2d7298425
TAYAGCH <tayagch@oocl.com>
2020-10-14 22:17:04 +0800

    [Practice01][Christian]update: update answer

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -3,6 +3,7 @@ package practice01;
 public class Person {
     private String name;
     private int age;
+
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
