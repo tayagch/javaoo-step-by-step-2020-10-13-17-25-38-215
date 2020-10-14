bb008d4093c746b164f000681e060b7122b65c5e
TAYAGCH <tayagch@oocl.com>
2020-10-14 16:05:10 +0800

    [Practice04][Christian]update: submit answer

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,22 @@
 package practice04;
 
-public class Student {
+public class Student extends Person{
+    private int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    public String introduce() {
+        return super.introduce() + " " +String.format("I am a Student. I am at Class %s.",klass);
+    }
 }
