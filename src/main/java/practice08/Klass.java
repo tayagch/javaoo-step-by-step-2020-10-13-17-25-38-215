e2b50940c40911520e42c6b4ecb6040384c0aa1a
TAYAGCH <tayagch@oocl.com>
2020-10-14 22:19:33 +0800

    [Practice08][Christian]update: submit answer

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,26 @@
 package practice08;
 
 public class Klass {
+    private int klass;
+    private Student leader;
+
+    public Klass(int klass) {
+        this.klass = klass;
+    }
+
+    public int getNumber() {
+        return klass;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %s",klass);
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Student leader) {
+        this.leader = leader;
+    }
 }
--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,31 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        return klass != null ? super.introduce()+ " " + "I am a Teacher. I teach Class 2." : super.introduce()+ " " + "I am a Teacher. I teach No Class.";
+    }
+
+    public String introduceWith(Student student) {
+        if(klass.getNumber() == student.getKlass().getNumber()){
+            return String.format("My name is Tom. I am 21 years old. I am a Teacher. I teach %s.",student.getName());
+        }else{
+            return String.format("My name is Tom. I am 21 years old. I am a Teacher. I don't teach %s.",student.getName());
+        }
+
+    }
 }
