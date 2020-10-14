3297a012ce5f40fd586dcc219e6a0e349f5cd333
TAYAGCH <tayagch@oocl.com>
2020-10-14 22:19:07 +0800

    [Practice07][Christian]update: submit answer

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,17 @@
 package practice07;
 
 public class Klass {
+    private int klass;
+
+    public Klass(int klass) {
+        this.klass = klass;
+    }
+
+    public int getNumber() {
+        return klass;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %s",klass);
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,31 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+    public Teacher(String name, int age, Klass klass) {
+        super(name,age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return klass != null ? super.introduce()+ " " + "I am a Teacher. I teach Class 2." : super.introduce()+ " " + "I am a Teacher. I teach No Class.";
+    }
+
+    public String introduceWith(Student student) {
+        if(klass.getNumber() == student.getKlass().getNumber()){
+            return String.format("My name is Tom. I am 21 years old. I am a Teacher. I teach %s.",student.getName());
+        }else{
+            return String.format("My name is Tom. I am 21 years old. I am a Teacher. I don't teach %s.",student.getName());
+        }
+
+    }
 }
